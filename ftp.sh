e507cf9dd48b6bc9ecc2a3bcf2ae5bd8d73d49d5
ANAKHA  BM <43615458+ceccs17d06@users.noreply.github.com>
2019-04-28 15:02:34 +0530

    SHELL SCRIPT

---
+++ ftp.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sftp cs17d06@192.168.0.30
+ls
+put ceccs17d4.2.19.txt
+
+put ceccs17d11.2.19.txt
+put ceccs17d25.2.19.txt
+get ceccs17d25.2.19.txt
+
+size
+status
+system
+
+pwd
+
+exit
+
+
